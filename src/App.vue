fa9c949de268c3ad78c15fe711199b49eeda87ec
yutaka.kakinuma <yutaka.kakinuma@alphacode.co.jp>
2020-03-16 19:36:02 +0900

    ツリー表示機能追加

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,83 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>(You can double click on an item to turn it into a folder.)</p>
+    <ul>
+      <TreeItem
+        class="item"
+        :item="treeData"
+        @make-folder="makeFolder"
+        @add-item="addItem"
+      >
+      </TreeItem>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TreeItem from './components/TreeItem.vue'
+import Vue from 'vue'
+
+var treeData = {
+  name: "My Tree",
+  children: [
+    { name: "hello" },
+    { name: "wat" },
+    {
+      name: "child folder",
+      children: [
+        {
+          name: "child folder",
+          children: [{ name: "hello" }, { name: "wat" }]
+        },
+        { name: "hello" },
+        { name: "wat" },
+        {
+          name: "child folder",
+          children: [{ name: "hello" }, { name: "wat" }]
+        }
+      ]
+    }
+  ]
+};
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TreeItem
+  },
+  data() {
+    return {
+      treeData: treeData
+    }
+  },
+  methods: {
+    makeFolder: function(item) {
+      Vue.set(item, "children", []);
+      this.addItem(item);
+    },
+    addItem: function(item) {
+      item.children.push({
+        name: "new stuff"
+      })
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  font-family: Menlo, Consolas, monospace;
+  color: #444;
+}
+.item {
+  cursor: pointer;
+}
+.bold {
+  font-weight: bold;
+}
+ul {
+  padding-left: 1em;
+  line-height: 1.5em;
+  list-style-type: dot;
 }
 </style>
